8840cb16ba6aeea169a76d08d3cf2bfd9f54f496
Dhanushka Samarakoon <blackpanthertourslk@gmail.com>
2024-02-08 23:48:01 +0530

    FizzBuzz updated

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,10 @@
+/*
+Objective: Write a function that prints the numbers from 1 to 100. 
+But for multiples of three, print "Fizz" instead of the number, and 
+for the multiples of five, print "Buzz". For numbers which are multiples 
+of both three and five, print "FizzBuzz".
+*/
+
 function FizzBuzz(i) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
